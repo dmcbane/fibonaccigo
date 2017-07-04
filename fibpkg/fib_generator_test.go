265e114917db5194704efd119fc3e02ef6d09170
H. Dale McBane <h.dale.mcbane@gmail.com>
2017-07-04 12:29:45 -0400

    Ensure generators are independent...

--- fibpkg/fib_generator_test.go
+++ fibpkg/fib_generator_test.go
@@ -6,9 +6,11 @@ import (
 
 func TestFibonacciGenerator(t *testing.T) {
 	gen := FibonacciGenerator()
+	gen2 := FibonacciGenerator() // ensure the generators are independent
 	for i := uint32(0); i < uint32(200); i++ {
 		expected := FibFastDoubling(i)
 		result := gen()
+		gen2() // call the other generator to ensure no interaction
 		if expected.Cmp(result) != 0 {
 			t.Errorf("Expected gen [fib('%v')] to be %v, got %v", i, expected, result)
 		}
